1153d635597b879df390adb10387ae9c2e3aa279
Gabriel de Souza Simplício <gabrieeel.08@gmail.com>
2025-09-30 21:09:00 -0300

    exercicio 011 finalizado

--- Exercicio011/app.js
+++ Exercicio011/app.js
@@ -6,19 +6,18 @@ function fnLimpar(){
 function fnSalvarNumero(){
     let numero = document.getElementById("numero").value
     listaDeNumeros.push(numero)
-    console.log("Funcionou? ",listaDeNumeros)
 }
 
 function fnCalcularMedia(){
     let media = 0, qtdNumeros = 0, somaDosNumeros = 0, i = 0
     qtdNumeros = listaDeNumeros.length
-    console.log("QtdNumeros: ", qtdNumeros)
     while(i<qtdNumeros){
         somaDosNumeros += listaDeNumeros[i]
         i++
     }
-    console.log("Soma: ", somaDosNumeros)
     media = somaDosNumeros / qtdNumeros
     document.getElementById("media").innerHTML = media
+
+    listaDeNumeros = []
 
 }
