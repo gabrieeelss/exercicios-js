658d975d79d2f484c6ffe1a9c1071128994dcd5f
Gabriel de Souza Simplício <gabrieeel.08@gmail.com>
2025-10-07 21:16:12 -0300

    index dos exercicios iniciada

---
+++ Exercicio002/app.js
@@ -0,0 +1,19 @@
+let n1 = 10, n2 = 2
+console.log(n1+n2) // + operador de adição
+document.getElementById("resultado_soma").innerText = n1+n2
+ 
+console.log(n1-n2) // - operador de subtração
+document.getElementById("resultado_subtracao").innerText = n1-n2
+ 
+console.log(n1*n2) // * operador de multi´licação
+document.getElementById("resultado_multiplicacao").innerText = n1*n2
+ 
+console.log(n1/n2) // / operador de divisão
+document.getElementById("resultado_divisao").innerText = n1/n2
+ 
+console.log(n1%n2) // % operador de modulo, pega o resto da divisão
+document.getElementById("resultado_modulo").innerText = n1%n2
+ 
+console.log(n1**n2) // ** operador de exponenciação, pega um numero e eleva a 10
+document.getElementById("resultado_exponenciacao").innerText = n1**n2
+ 
