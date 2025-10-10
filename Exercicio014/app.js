2cb7b9bf16f78d4cb76726bb919b3247e4f6f2cd
Gabriel de Souza Simplício <gabrieeel.08@gmail.com>
2025-10-09 21:26:08 -0300

    exercicio 014 finalizado

--- Exercicio014/app.js
+++ Exercicio014/app.js
@@ -1,54 +1,55 @@
 const times = [
-  "Atlético Mineiro",
-  "Bahia",
-  "Botafogo",
-  "Ceará",
-  "Corinthians",
-  "Cruzeiro",
-  "Flamengo",
-  "Fluminense",
-  "Fortaleza",
-  "Grêmio",
-  "Internacional",
-  "Juventude",
-  "Mirassol",
-  "Palmeiras",
-  "Red Bull Bragantino",
-  "Santos",
-  "São Paulo",
-  "Sport",
-  "Vasco da Gama",
-  "Vitória"
+    "Atlético Mineiro",
+    "Bahia",
+    "Botafogo",
+    "Ceará",
+    "Corinthians",
+    "Cruzeiro",
+    "Flamengo",
+    "Fluminense",
+    "Fortaleza",
+    "Grêmio",
+    "Internacional",
+    "Juventude",
+    "Mirassol",
+    "Palmeiras",
+    "Red Bull Bragantino",
+    "Santos",
+    "São Paulo",
+    "Sport",
+    "Vasco da Gama",
+    "Vitória"
 ];
 
 const mascoteTimes = [
-  "Galo" ,               // Atlético Mineiro
-  "Super-Homem Tricolor",// Bahia
-  "Manequinho",           // Botafogo
-  "Vovô",                // Ceará
-  "Mosqueteiro",          // Corinthians
-  "Raposa",              // Cruzeiro
-  "Urubu",               // Flamengo
-  "Pó de Arroz",         // Fluminense
-  "Leão",                // Fortaleza
-  "Mosqueteiro",         // Grêmio
-  "Saci",                // Internacional
-  "Periquito",           // Juventude
-  "Leão",                // Mirassol
-  "Porco",               // Palmeiras
-  "Toro Loko",           // Red Bull Bragantino
-  "Baleia",              // Santos
-  "Soberano" ,           // São Paulo
-  "Leão",                // Sport
-  "Almirante",           // Vasco da Gama
-  "Leão",                // Vitória
+    "Galo",               // Atlético Mineiro
+    "Super-Homem Tricolor",// Bahia
+    "Manequinho",           // Botafogo
+    "Vovô",                // Ceará
+    "Mosqueteiro",          // Corinthians
+    "Raposa",              // Cruzeiro
+    "Urubu",               // Flamengo
+    "Pó de Arroz",         // Fluminense
+    "Leão",                // Fortaleza
+    "Mosqueteiro",         // Grêmio
+    "Saci",                // Internacional
+    "Periquito",           // Juventude
+    "Leão",                // Mirassol
+    "Porco",               // Palmeiras
+    "Toro Loko",           // Red Bull Bragantino
+    "Baleia",              // Santos
+    "Soberano",           // São Paulo
+    "Leão",                // Sport
+    "Almirante",           // Vasco da Gama
+    "Leão",                // Vitória
 ];
 
-function fnListaTimes(){
-    document.getElementById("lista_times").innerHTML = `
-    <div class="col-6 border rounded text-center"> ${times[0]} - ${mascoteTimes[0]}`
+function fnListaTimes() {
+    for (let i = 0; i < times.length; i++) {
+        document.getElementById("lista_times").innerHTML += `
+    <div class="col-6 border rounded text-center"> ${times[i]} - ${mascoteTimes[i]}`
+    }
 }
-
 fnListaTimes()
 
 console.dir(times)
